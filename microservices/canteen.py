ebecdbeb32152c9e8709d877cd1bb3698864c315
rubengomes8 <ruben.gomes@tecnico.ulisboa.pt>
2019-11-24 22:47:07 +0000

    cantina to do

--- microservices/canteen.py
+++ microservices/canteen.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import request
+import requests
 '''
 endpoints:
 - "api/canteen" apresenta menu de opções do microserviço cantina
@@ -18,8 +19,25 @@ app = Flask(__name__)
 def canteen_menu():
     return render_template("canteenMenu.html")
 
-@app.route('/api/canteen/week')
+@app.route('/api/canteen/week', methods=['GET'])
 def week_info():
+    if request.method == "GET":
+        url = 'https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen/'
+        r = requests.get(url=url)
+        data = r.json()
+        print(data)
+        days = []
+        _meals = {}
+        for day in data:
+            days.append(day['day'])
+            for meals in day['meal']['info']:
+                type1 = day['meal']['type']
+                for meal in meals:
+                    meals[type1] = [meal['type'], meal['name']]
+
+        print(days)
+        print(_meals)
+        
     return render_template("weeklyCanteenInfo.html")
 
 @app.route('/api/canteen/day')
